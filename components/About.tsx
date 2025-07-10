6d04554f1ae512cb68878e6355ae9dd3739f0fe9
Alvin Dennis <alvindennis80@gmail.com>
2025-07-10 10:50:48 +0530

    fix: performance issues

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { Card } from "@/components/ui/card";
 import { aboutUs } from "@/lib/constants";
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { Card } from "@/components/ui/card";
 import { services } from "@/lib/constants";
